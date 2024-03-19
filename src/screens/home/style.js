6a517dc61390d05e1b7464121d43e434b3865dcf
SHIFTLABOR\crodriguez <crodriguez@shiftlabor.com>
2024-03-18 22:46:08 -0500

    Feat [OrganizacionArchivos]: Se establecion la forma de como se trabajara.

---
+++ src/screens/home/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F2F3F8",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
